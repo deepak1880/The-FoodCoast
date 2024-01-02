82a7973caeca9ed557f19053c530ab6520b8346d
Deepak Mandal <mandaldeepak174@gmail.com>
2024-01-02 11:40:49 +0530

    Final Commmit

--- app/src/main/java/com/example/thefoodcoast/fragment/FavouriteFragment.kt
+++ app/src/main/java/com/example/thefoodcoast/fragment/FavouriteFragment.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.thefoodcoast.R
 
 
 class FavouriteFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        argumgit push -u origin mainents?.let {
+        arguments?.let {
 
         }
     }
